2728253b530a536cc0db7e7147d12ee00bcaa23a
JKCamus <924850758@qq.com>
2021-09-13 18:20:35 +0800

    feat: async mounted test

--- react-ts/src/App.tsx
+++ react-ts/src/App.tsx
@@ -1,5 +1,5 @@
+// import React from 'react';
 import { HashRouter, Route, Switch } from 'react-router-dom';
-
 import routes from './router';
 
 function App() {
---
+++ react-ts/src/pages/Enjoy/index.ts
@@ -0,0 +1,8 @@
+// TODO导出模块合集 =>https://developer.mozilla.org/zh-CN/docs/Web/JavaScript/Reference/Statements/export
+// export * from …; // does not set the default export
+// export * as name1 from …; // Draft ECMAScript® 2O21
+// export { name1, name2, …, nameN } from …;
+// export { import1 as name1, import2 as name2, …, nameN } from …;
+// export { default } from …;
+
+export { default as AsyncUnMount } from './components/AsyncUnMount';
