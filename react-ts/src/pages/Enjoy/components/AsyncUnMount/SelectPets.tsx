5f5a33f8c1d2f8857688e43de3f8cd024f26d0e8
JKCamus <924850758@qq.com>
2021-09-14 15:20:11 +0800

    feat: bug解决

--- react-ts/src/pages/Enjoy/components/AsyncUnMount/SelectPets.tsx
+++ react-ts/src/pages/Enjoy/components/AsyncUnMount/SelectPets.tsx
@@ -4,15 +4,18 @@
  * @Author: camus
  * @Date: 2021-09-13 16:38:56
  * @LastEditors: camus
- * @LastEditTime: 2021-09-14 11:37:15
+ * @LastEditTime: 2021-09-14 14:13:03
  */
 import React, { useEffect, useReducer } from 'react';
 import { initialState, petsReducer, getData, Types } from './utils';
 import { Select } from 'antd';
 import Pets from './Pets';
+import { useUnmountedRef } from 'ahooks';
+
 const { Option } = Select;
 
 const SelectPets: React.FC = (props) => {
+  const unmountRef: { current: boolean } = useUnmountedRef();
   const [pets, dispatch] = useReducer(petsReducer, initialState);
 
   const onChange = (value: string) => {
@@ -23,7 +26,9 @@ const SelectPets: React.FC = (props) => {
     if (pets.selectedPet) {
       dispatch({ type: Types.FetchPet });
       getData(pets.selectedPet).then((data) => {
-        dispatch({ type: Types.FetchPetSuccess, payload: data });
+        if (!unmountRef.current) {
+          dispatch({ type: Types.FetchPetSuccess, payload: data });
+        }
       });
     } else {
       dispatch({ type: Types.Rest });
