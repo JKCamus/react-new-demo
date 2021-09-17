7b073307a3b6b0399f4dea4a1d1dd1c3b24e22b9
JKCamus <924850758@qq.com>
2021-09-17 17:24:08 +0800

    feat: slotdemo

---
+++ react-ts/src/pages/Enjoy/components/SlotDemo/SlotDemo.tsx
@@ -0,0 +1,36 @@
+/*
+ * @Description:
+ * @version:
+ * @Author: camus
+ * @Date: 2021-09-01 19:30:47
+ * @LastEditors: camus
+ * @LastEditTime: 2021-09-15 19:10:51
+ */
+import React, { ReactNode } from 'react';
+import styled from 'styled-components';
+import { Card } from 'antd';
+import Footer from './Footer';
+import Header from './Header';
+
+interface IProps {
+  className?: string;
+  footer?: ReactNode;
+  header?: ReactNode;
+}
+
+const SlotDemo: React.FC<IProps> = ({ footer, header }) => {
+  return (
+    <SlotDemoWrapper>
+      <Card>
+        card
+        <Header>{header}</Header>
+        <Footer>{footer}</Footer>
+      </Card>
+    </SlotDemoWrapper>
+  );
+};
+export default SlotDemo;
+
+const SlotDemoWrapper = styled.div`
+  background-color: #fff;
+`;
