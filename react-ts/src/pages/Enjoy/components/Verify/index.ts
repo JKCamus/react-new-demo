53fbbb23cad24a1f64c0a7ee4f1ab9e17b90c958
JKCamus <924850758@qq.com>
2021-09-18 15:12:07 +0800

    feat: verfy cpn

---
+++ react-ts/src/pages/Enjoy/components/Verify/index.ts
@@ -0,0 +1,9 @@
+/*
+ * @Description:
+ * @version:
+ * @Author: camus
+ * @Date: 2021-09-17 16:06:36
+ * @LastEditors: camus
+ * @LastEditTime: 2021-09-17 16:07:43
+ */
+export { default as Verify } from './main';
