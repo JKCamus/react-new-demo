87a4a64cae900a0a0ad0963ed7d12d623e673a3e
JKCamus <924850758@qq.com>
2021-10-14 17:19:51 +0800

    feat: test

---
+++ react-ts/src/components/ErrorComponent/index.ts
@@ -0,0 +1,10 @@
+/*
+ * @Description:
+ * @version:
+ * @Author: camus
+ * @Date: 2021-09-07 16:35:27
+ * @LastEditors: camus
+ * @LastEditTime: 2021-10-14 15:03:21
+ */
+
+export { default as ErrorFallback } from './ErrorFallback';
