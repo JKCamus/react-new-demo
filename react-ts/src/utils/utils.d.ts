85e2134318618ff190a719cc72e404d06062a069
JKcamus <924850758@qq.com>
2021-10-21 17:29:39 +0800

    feat：type 限定数组的类型和个数

--- react-ts/src/utils/utils.d.ts
+++ react-ts/src/utils/utils.d.ts
@@ -16,3 +16,14 @@ export type SetRequired<T, K extends keyof T> = Simplify<Pick<T, Exclude<keyof T
  * @description: 定义非空数组
  */
 export type NonEmptyArray<T> = [T, ...T[]];
+
+/**
+ * @description: 定义数组类型与个数
+ */
+type Grow<T, A extends Array<T>> = ((x: T, ...xs: A) => void) extends (...a: infer X) => void ? X : never;
+type GrowToSize<T, A extends Array<T>, N extends number> = {
+  0: A;
+  1: GrowToSize<T, Grow<T, A>, N>;
+}[A['length'] extends N ? 0 : 1];
+
+export type FixedArray<T, N extends number> = GrowToSize<T, [], N>;
