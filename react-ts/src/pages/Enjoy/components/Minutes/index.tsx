5073b3d253335d5d49bf23fb052778327440a240
JKcamus <924850758@qq.com>
2021-11-13 10:09:34 +0800

    test: change

--- react-ts/src/pages/Enjoy/components/Minutes/index.tsx
+++ react-ts/src/pages/Enjoy/components/Minutes/index.tsx
@@ -4,11 +4,16 @@
  * @Author: camus
  * @Date: 2021-10-18 09:33:46
  * @LastEditors: camus
- * @LastEditTime: 2021-10-18 09:33:47
+ * @LastEditTime: 2021-10-18 10:00:39
  */
 import React from 'react';
+import MinuteCard from './MinuteCard';
 
 const Minutes: React.FC = (props) => {
-  return <div>Minutes</div>;
+  return (
+    <div>
+      <MinuteCard />
+    </div>
+  );
 };
 export default Minutes;
