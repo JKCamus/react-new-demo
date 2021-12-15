2bf45a7a1090aff649858eb31e70fa5cfc38ceb6
JKcamus <924850758@qq.com>
2021-12-15 14:51:22 +0800

    feat: useImage 版本更新，收集表类图

--- react-ts/src/pages/Enjoy/components/ImgLoadDemo/util/useImage.ts
+++ react-ts/src/pages/Enjoy/components/ImgLoadDemo/util/useImage.ts
@@ -1,6 +1,14 @@
-import * as React from 'react';
+/*
+ * @Description:经过协作版本改良后
+ * @version:
+ * @Author: camus
+ * @Date: 2021-11-13 10:11:47
+ * @LastEditors: camus
+ * @LastEditTime: 2021-12-15 14:50:14
+ */
+import { useSafeState } from 'ahooks';
+import React, { useEffect, useLayoutEffect, useState } from 'react';
 
-// 将图片加载转为promise调用形式
 const imgPromise = (src: string): Promise<void> => {
   return new Promise((resolve, reject) => {
     const i = new Image();
@@ -50,25 +58,28 @@ const useImage = ({
   loadImg = imgPromise,
   srcList,
 }: useImageParams): { src: string | undefined; loading: boolean; error: any } => {
-  const [loading, setLoading] = React.useState(true);
-  const [error, setError] = React.useState(null);
-  const [value, setValue] = React.useState<string | undefined>(undefined);
+  // 此处用到useSafeState 为了防止在数据未到进行渲染，引起warning。
+  const [loading, setLoading] = useSafeState(true);
+  const [error, setError] = useSafeState(null);
+  const [value, setValue] = useSafeState<string | undefined>(undefined);
 
   const sourceList = removeBlankArrayElements(stringToArray(srcList));
   const sourceKey = sourceList.join('');
 
-  React.useEffect(() => {
+  // 此处由原来的useEffect改为useLayoutEffect，原因： 避免虚拟列表中图片或者图片列表加载时的闪动
+  useLayoutEffect(() => {
     if (!cache[sourceKey]) {
       cache[sourceKey] = promiseFind(sourceList, loadImg);
     }
-
     cache[sourceKey]
       .then((src) => {
         setLoading(false);
         setValue(src);
+        setError(null);
       })
       .catch((error) => {
         setLoading(false);
+        setValue('');
         setError(error);
       });
   }, [sourceKey]);
