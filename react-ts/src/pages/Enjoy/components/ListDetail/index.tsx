e1a5d3e7d59607382cd1334bd0aa912cb214d213
JKcamus <924850758@qq.com>
2021-12-27 17:20:45 +0800

    feat: useWatch test

--- react-ts/src/pages/Enjoy/components/ListDetail/index.tsx
+++ react-ts/src/pages/Enjoy/components/ListDetail/index.tsx
@@ -4,11 +4,21 @@
  * @Author: camus
  * @Date: 2021-10-11 10:26:58
  * @LastEditors: camus
- * @LastEditTime: 2021-10-13 16:48:50
+ * @LastEditTime: 2021-12-15 14:59:36
  */
-import React from 'react';
+import React, { useState } from 'react';
+import useWatch from './useWatch';
 
 const ListDetail: React.FC = (props) => {
-  return <div>detail</div>;
+  const [num, setNum] = useState(1);
+
+  useWatch(num, (pre) => console.log(pre, num), { immediate: true });
+
+  return (
+    <div>
+      <div style={{ color: 'red' }}>{num}</div>
+      <button onClick={() => setNum(num + 1)}>点我</button>
+    </div>
+  );
 };
 export default ListDetail;
